cba26d65dafaca522abe25677f352141982e207f
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-02-23 12:21:25 +0330

    add campGround seeds

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Database Connected');
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log('Database Connected');
+    })
+    .catch(err => {
+        console.log('Connection Error');
+        console.log(err);
+    });
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
---
+++ seeds/index.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const CampGround = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log('Database Connected');
+    })
+    .catch(err => {
+        console.log('Connection Error');
+        console.log(err);
+    });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await CampGround.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const camp = new CampGround({
+            title: `${sample(descriptors)} ${sample(places)}`,
+            location: `${sample(cities).city}, ${sample(cities).state}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.',
+            price: Math.floor(Math.random() * 20) + 10
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
