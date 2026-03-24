9905b1b1ca9c938bf6448c6e141e93cb24262c36
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-03-24 18:19:37 +0330

    add public (static) folder

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const reviews = require('./routes/reviews');
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/campgrounds', campGrounds);
