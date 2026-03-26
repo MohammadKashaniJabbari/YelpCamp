9fb044f377cc5dbd305a4401f20a8965564cc769
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-03-27 03:23:04 +0330

    add login after register

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
         const user = new User({ username, email });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        });
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
@@ -31,10 +34,8 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 });
 
 router.get('/logout', isLoggedIn, (req, res) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
+    req.logout(err => {
+        if (err) return next(err);
         req.flash('success', 'Goodbye!');
         res.redirect('/campgrounds');
     });
