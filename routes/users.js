36dabf8efc2d15711dd1c43541e3c4a254a588d3
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-03-27 02:37:54 +0330

    add logout

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
+const { isLoggedIn } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -27,6 +28,16 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome Back!');
     res.redirect('/campgrounds');
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
 });
 
 module.exports = router;
