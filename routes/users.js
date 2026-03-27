4e5abfb8c3ee13c1ee1e7be572b4a7828a7c9eb9
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-03-27 04:16:28 +0330

    add back to the same page after login

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -28,9 +28,10 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome Back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout', isLoggedIn, (req, res) => {
