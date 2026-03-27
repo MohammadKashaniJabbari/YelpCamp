3f45a7c4943e2aac8a4d037fa35795e8ace5a0fa
MohammadKashaniJabbari <h.kashani2001@gmail.com>
2026-03-27 17:51:36 +0330

    group same routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const { isLoggedIn, validateCampGround, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 
-router.get('/', campgrounds.index);
+router.route('/')
+    .get(campgrounds.index)
+    .post(isLoggedIn, validateCampGround, campgrounds.createCampGround);
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampGround, campgrounds.createCampGround);
-
-router.get('/:id', campgrounds.showCampGround);
+router.route('/:id')
+    .get(campgrounds.showCampGround)
+    .put(isLoggedIn, isAuthor, validateCampGround, campgrounds.updateCampGround)
+    .delete(isLoggedIn, isAuthor, campgrounds.deleteCampGround);
 
 router.get('/:id/edit', isLoggedIn, isAuthor, campgrounds.renderEditForm);
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampGround, campgrounds.updateCampGround);
-
-router.delete('/:id', isLoggedIn, isAuthor, campgrounds.deleteCampGround);
 
 module.exports = router;
